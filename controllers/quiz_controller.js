a61873bef4e4ea19b6c999e718f547bb99eabed4
Atrus-sama <juosra2000@gmail.com>
2015-06-09 14:36:17 +0200

    Ejercicio P2P modulo 7 - Busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,11 +19,21 @@ exports.load = function (req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function (req, res) {
-  models.Quiz.findAll().then(function (quizes) {
-    res.render("quizes/index.ejs", {quizes: quizes});
-  }).catch(function(error) {
-    next(error);
-  });
+  if (req.query.search !== undefined && req.query.search !== ""){
+    var search = "%" + req.query.search.replace(" ", "%") + "%";
+    models.Quiz.findAll({where: ["lower(pregunta) like ?", search.toLowerCase()]}).then(function (quizes) {
+      res.render("quizes/index.ejs", {quizes: quizes});
+    }).catch(function(error) {
+      next(error);
+    });
+  }
+  else {
+    models.Quiz.findAll().then(function (quizes) {
+      res.render("quizes/index.ejs", {quizes: quizes});
+    }).catch(function(error) {
+      next(error);
+    });
+  }
 }
 
 // GET /quizes/question
